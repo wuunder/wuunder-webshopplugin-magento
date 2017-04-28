c9f842aa2d7c71b1db60b658fea43a5f53d858fd
Timo <tj.timo.j@gmail.com>
2017-04-29 00:28:45 +0200

    Revert "."
    This reverts commit 14aade0458d9c499111b1111da404f2327ae56b7.

--- app/code/community/Wuunder/WuunderConnector/Model/Observer.php
+++ app/code/community/Wuunder/WuunderConnector/Model/Observer.php
@@ -13,20 +13,4 @@ class Wuunder_WuunderConnector_Model_Observer extends Varien_Event_Observer {
 			array('label_id','label_url','label_tt_url','retour_id','retour_url','retour_tt_url')
 		);
 	}
-
-    public function adminhtmlWidgetContainerHtmlBefore($event)
-    {
-        $block = $event->getBlock();
-        $orderId = $block->getOrderId();
-        $linkurl  = Mage::helper('adminhtml')->getUrl('adminhtml/wuunder/create/id/', array('id' => $orderId));
-
-        if ($block instanceof Mage_Adminhtml_Block_Sales_Order_View) {
-//            $message = Mage::helper('wuunderconnector')->__('Are you sure you want to do this?');
-            $block->addButton('order_ship', array(
-                'label'     => Mage::helper('sales')->__('Ship'),
-                'onclick'   => 'setLocation(\'' . $linkurl . '\')',
-                'class'     => 'go'
-            ), 0, 40);
-        }
-    }
 }
