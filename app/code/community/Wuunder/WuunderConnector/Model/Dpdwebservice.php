9917000362d7a8267727c2ee3f7bdb96a759524d
Timo <tj.timo.j@gmail.com>
2017-09-13 08:39:55 +0200

    .

--- app/code/community/Wuunder/WuunderConnector/Model/Dpdwebservice.php
+++ app/code/community/Wuunder/WuunderConnector/Model/Dpdwebservice.php
@@ -351,6 +351,7 @@ class Wuunder_Wuunderconnector_Model_Dpdwebservice extends Mage_Core_Model_Abstr
         );
 
         $result = $this->_webserviceCall($webserviceUrl, 'findParcelShopsByGeoData', $parameters);
+//        Mage::helper('wuunderconnector')->log($result);
         return $result;
 
     }
